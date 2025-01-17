07cb0d5e6e2c39a484120fb6dd5e758fb572966e
Olga-Stebko <78696791+Olga-steblyk@users.noreply.github.com>
2025-01-17 01:56:07 +0000

    question1 fix

--- question1.js
+++ question1.js
@@ -4,4 +4,4 @@ let x, y;
 x = 7; //this is the input for the KM
 y= x * 0.62137;
 
-console.log(y +" is the mile equivalent to " + x + "km.");
+console.log(`${y} is the mile equivalent to ${x}km.`);
